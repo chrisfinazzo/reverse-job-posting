b2a41b94d13a9a18357d1856a5481a16b71a8fda
chrisfinazzo <chris.finazzo@gmail.com>
2024-10-10 10:54:29 -0400

    Eleventy v3

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,11 @@
-module.exports = function (eleventyConfig) {
-eleventyConfig.addPassthroughCopy("./src/css/app.css");
-eleventyConfig.addPassthroughCopy("./src/media");
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css/app.css");
+  eleventyConfig.addPassthroughCopy("./src/media");
+
   return {
     dir: {
       input: "src",
       output: "public"
-    },
+    }
   };
-};
+}
